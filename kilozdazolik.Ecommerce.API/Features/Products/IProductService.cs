01f35b81b2ae70629fb17ee1cc80af7ab0548520
kilozdazolik <paulsonzoltan@protonmail.com>
2026-01-16 17:37:28 +0100

    Add product service interface and DTOs
    Introduced IProductService interface with CRUD methods for products.
    Added ProductDto, CreateProductDto, and UpdateProductDto records
    to define data structures for product management features.

---
+++ kilozdazolik.Ecommerce.API/Features/Products/IProductService.cs
@@ -0,0 +1,12 @@
+﻿namespace kilozdazolik.Ecommerce.API.Features.Products
+{
+    public interface IProductService
+    {
+        Task<ProductDto> CreateProductAsync(CreateProductDto product);
+        Task<ProductDto?> GetProductByIdAsync(Guid id, int pageIndex = 1 , int pageSize = 10);
+        Task<IEnumerable<ProductDto>> GetProductsAsync();
+        Task UpdateProductAsync(Guid id, UpdateProductDto product);
+        Task DeleteProductAsync(Guid id);
+        Task RestoreProductAsync(Guid id);
+    }
+}
---
+++ kilozdazolik.Ecommerce.API/Features/Products/ProductDtos.cs
@@ -0,0 +1,20 @@
+﻿namespace kilozdazolik.Ecommerce.API.Features.Products
+{
+    public record ProductDto(
+    Guid Id,
+    Guid CategoryId,
+    string CategoryName,
+    string Name,
+    decimal Price,
+    bool IsDeleted);
+
+    public record CreateProductDto (
+        Guid CategoryId,
+        string Name,
+        decimal Price);
+
+    public record UpdateProductDto (
+        Guid CategoryId,
+        string Name,
+        decimal Price);
+}
